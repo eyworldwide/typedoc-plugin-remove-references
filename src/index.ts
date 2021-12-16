12de2b277114cee9e70b60a2d1579f6e18de4aac
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2021-12-16 22:23:23 +0900

    temporary fix: type error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Application, ReflectionKind,Converter } from 'typedoc';
 export function load({ application }: { application: Application }) {
-  application.converter.on(Converter.EVENT_RESOLVE_BEGIN, context => {
+  application.converter.on(Converter.EVENT_RESOLVE_BEGIN, (context:any) => {
     for (const reflection of context.project.getReflectionsByKind(ReflectionKind.Reference)) {
       context.project.removeReflection(reflection)
     }
